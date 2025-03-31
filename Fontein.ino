b206a88f01fe17e17ce90fc927daa4a2e5a5f9e9
Duco Fijma <duco@fijma.net>
2025-03-31 19:17:49 +0200

    working timor on leds

--- Fontein.ino
+++ Fontein.ino
@@ -23,6 +23,8 @@ const int pixelCount = 10;
 
 Adafruit_NeoPixel strip(pixelCount, pixelPin, NEO_GRB + NEO_KHZ800);
 
+int b[pixelCount];
+
 // clock, display and RTC
 
 Adafruit_7segment matrix = Adafruit_7segment();
@@ -67,6 +69,9 @@ void setup() {
   strip.begin();
   strip.show();
   strip.setBrightness(50);
+  for (int i=0; i < pixelCount; i++) {
+    b[i] = random(256);
+  }
 
   pinMode(relayPin, OUTPUT);
 
@@ -86,21 +91,21 @@ void loop() {
   if (dark) {
     if (light > LIGHT_THRESHOLD + (hysteresis / 2)) {
       dark = false;
-      Serial.print("went light ");
+      //Serial.print("went light ");
     }
   } else {
     if (light < LIGHT_THRESHOLD - (hysteresis / 2)) {
       dark = true;
-      Serial.print("went dark ");
+      // Serial.print("went dark ");
     }
   }
 
+  // DEBUG
   Serial.print(hysteresis);
   Serial.print(" ");
   Serial.println(light);
 
-  digitalWrite(ledPin, dark);
-  digitalWrite(relayPin, dark);
+
 
   // get input and current time
   int button1 = digitalRead(BUTTON_1_PIN);
@@ -193,7 +198,7 @@ void loop() {
   // set RTC from buffer
   if (setRTC) {
     // we actually don't care about the date
-    rtc.adjust(DateTime(2020,1,1, hours, minutes, 0));
+    rtc.adjust(DateTime(2020, 1, 1, hours, minutes, 0));
   }
 
   // update display   
@@ -205,57 +210,30 @@ void loop() {
     matrix.drawColon(now.second() % 2 == 0);   
   }
 
-  Serial.print(now.hour()); Serial.print(":"); Serial.println(now.minute());
+  // DEBUG
+  // Serial.print(now.hour()); Serial.print(":"); Serial.println(now.minute());
 
   matrix.writeDisplay();
 
-  if (dark && !showBufferTime ) {
-    theaterChaseRainbowStep();
+  if (dark && !showBufferTime && (now.hour() >= 6) ) {
+    neoPixelStep();
   } else {
     strip.clear();
     strip.show();
   }
+  
+  digitalWrite(ledPin, dark );
+  digitalWrite(relayPin, true);
 
   delay(50);
 }
 
-void theaterChaseRainbow(int wait) {
-  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
-  for(int aa=0; aa<30; aa++) {  // Repeat 30 times...
-    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
-      // 'c' counts up from 'b' to end of strip in increments of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
-        // hue of pixel 'c' is offset by an amount to make one full
-        // revolution of the color wheel (range 65536) along the length
-        // of the strip (strip.numPixels() steps):
-        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
-        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
-        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
-      }
-      strip.show();                // Update strip with new contents
-      delay(wait);                 // Pause for a moment
-      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
-    }
-  }
-}
+void neoPixelStep() {
+  for (int i = 0; i<pixelCount; i++) {
+    strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(180*256, b[i], b[i])));
 
-
-int firstPixelHue = 0; 
-int b = 0;
-
-void theaterChaseRainbowStep() {
-  strip.clear();         
-  for (int c=b; c<strip.numPixels(); c += 3) {
-    int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
-    uint32_t color = strip.gamma32(strip.ColorHSV(hue)); 
-    strip.setPixelColor(c, color); 
+    b[i]--;
+    if (b[i]<0) b[i] = random(256);
   }
-  strip.show(); 
-  firstPixelHue += 65536 / 90; 
-  b++;
-  if (b > 3) { 
-    b = 0;
-    firstPixelHue = 0;
-  }
+  strip.show();
 }
